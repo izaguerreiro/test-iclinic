9752ffa02ea69e0df36e824230b9571bf044cfc2
Izabela Guerreiro <izaguerreiro@gmail.com>
2018-02-28 15:55:46 -0300

    Create a model Schedule

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Schedule(models.Model):
+    date = models.DateField('data')
+    start_time = models.TimeField('horário de início')
+    end_time = models.TimeField('horário final')
+    patient = models.CharField('paciente', max_length=255)
+    procedure = models.TextField('procedimento')
+
+    def __str__(self):
+        return self.patient
+
+    class Meta:
+        verbose_name = 'agenda'
+        verbose_name_plural = 'agendas'
