7bde8baf7f72b03c201ae3abc06736d4105a2c18
Izabela Guerreiro <izaguerreiro@gmail.com>
2018-03-01 14:54:00 -0300

    Test post in API

---
+++ schedule/tests/test_api_post_schedule.py
@@ -0,0 +1,27 @@
+""" Test post in API """
+
+from json import dumps
+from django.test import TestCase
+
+
+class TestPostSchedule(TestCase):
+    """ Test post schedule """
+
+    def setUp(self):
+        data = {
+            'date': '2018-02-28', 'start_time': '10:00:00',
+            'end_time': '11:00:00', 'patient': 'Izabela Guerreiro',
+            'procedure': 'Consulta de rotina'
+        }
+        self.response = self.client.post(
+            '/api/schedules/', dumps(data),
+            content_type='application/json'
+        )
+
+    def test_status_code(self):
+        """ Check request status """
+        self.assertEqual(201, self.response.status_code)
+
+    def test_response_content_type(self):
+        """ Check the submitted content type """
+        self.assertEqual('application/json', self.response.get('Content-Type'))
